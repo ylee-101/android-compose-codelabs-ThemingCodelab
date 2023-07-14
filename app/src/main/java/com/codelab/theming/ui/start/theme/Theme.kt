b8f4e9ad46e2d8a22182ed08add7184916a95d58
Y.LEE <y.lee@101.inc>
2023-07-14 15:01:09 +0900

    테마에 color 셋팅

---
+++ app/src/main/java/com/codelab/theming/ui/start/theme/Theme.kt
@@ -0,0 +1,27 @@
+package com.codelab.theming.ui.start.theme
+
+import Red700
+import Red800
+import Red900
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val LightColors = lightColors(
+    primary = Red700,
+    primaryVariant = Red900,
+    onPrimary = Color.White,
+    secondary = Red700,
+    secondaryVariant = Red900,
+    onSecondary = Color.White,
+    error = Red800
+)
+
+@Composable
+fun JetnewsTheme(content : @Composable () -> Unit ) {
+    MaterialTheme(
+        content = content,
+        colors = LightColors
+    )
+}
